feac18500243e16ae470baeb80ca4dc0079303b9
Mell-A <melaniearp@hotmail.com>
2024-01-24 18:32:28 +0100

    Add login_cluey()

---
+++ R/login.R
@@ -0,0 +1,20 @@
+login_cluey <- function(username = "", password = "", url = "https://focus.sensingclues.org/") {
+  # login
+  url_login <- paste0(url, "api/auth/login")
+  json_body <- jsonlite::toJSON(list(username = username, password = password),
+                                auto_unbox = TRUE)
+  # we set up an authenticated session
+  # force empty cookie (new handle pool)
+  httr::handle_reset(url_login)
+  rl <- httr::POST(url_login, body = json_body, encode = "raw", httr::content_type_json()) # verbose())
+
+  # for logging
+  message("Login attempt by ", username)
+  if (httr::status_code(rl) == 200) {
+    message("Successfully logged in")
+    return(httr::cookies(rl))
+  } else {
+    warning("Login failed!")
+    return(NULL)
+  }
+}
