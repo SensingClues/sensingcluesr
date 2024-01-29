68f4ef93a4a8e6e648c203f6c4ee862f2a6a9428
Mell-A <melaniearp@hotmail.com>
2024-01-29 23:46:29 +0100

    Add get_groups()

---
+++ R/groups.R
@@ -0,0 +1,44 @@
+get_groups <- function(cookie,
+                       from = "1900-01-01",
+                       to = "2999-12-31",
+                       url = "https://focus.sensingclues.org/") {
+  # facets
+  url_search_results <- paste0(url, "api/search/all/facets")
+  query <- paste0('
+    {"filters":
+        {"dateTimeRange":
+            {"to":"', to, 'T24:00:00.000Z",
+              "from":"', from, 'T00:00:00.000Z"
+            },
+          "entities":["Observation","track"]
+        },
+      "options": {
+        "start": 1,
+        "pageLength": 0
+      }
+    }
+  ')
+  httr::handle_reset(url_search_results)
+  result <- httr::POST(url_search_results, body = query, encode = "raw",
+                       httr::content_type_json(), httr::set_cookies(focus2 = utils::URLdecode(cookie$value))) # verbose())
+  facet_result <- httr::content(result)
+  # unpack the content
+  data_sources <- facet_result$facets$dataSources
+  Ngroups <- length(data_sources$facetValues)
+  GROUPS <- data.frame()
+  if(Ngroups != 0) {
+    for (i in 1:Ngroups) {
+      # get to content
+      name <- data_sources$facetValues[[i]]$name
+      count <- data_sources$facetValues[[i]]$count
+      value <- data_sources$facetValues[[i]]$value
+
+      GROUPS <- rbind(GROUPS, c(name, count, value))
+      # names
+      names(GROUPS) <- c("name", "count", "value")
+    }
+  } else {
+    message("No groups received from backend, returning empty dataframe")
+  }
+  return(GROUPS)
+}
