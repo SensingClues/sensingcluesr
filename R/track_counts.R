66526686f2e0e42ae8e02de809b8789f859c3de4
Mell-A <melaniearp@hotmail.com>
2026-03-04 16:28:26 +0100

    Improve get_track_counts() and add hierarchy parameter

--- R/track_counts.R
+++ R/track_counts.R
@@ -4,6 +4,7 @@
 #' @param group One or multiple group identification character string(s), see which groups you have access to with [get_groups()].
 #' @param from Start date.
 #' @param to End date.
+#' @param hierarchy A Sensing Clues knowledge graph object, to be retrieved with [get_hierarchy()].
 #' @param url A Sensing Clues URL, default is [https://focus.sensingclues.org/](https://focus.sensingclues.org/).
 #' @param lang Language in which the concepts are shown, default is English.
 #'
@@ -17,6 +18,7 @@ get_track_counts <- function(cookie,
                              group,
                              from = "1900-01-01",
                              to = "2999-12-31",
+                             hierarchy = NULL,
                              url = "https://focus.sensingclues.org/",
                              lang = "en") {
   # ontology counts
@@ -40,23 +42,16 @@ get_track_counts <- function(cookie,
   result <- httr::POST(url_search_results, body = query, encode = "raw", httr::content_type_json(), httr::set_cookies(focus2 = utils::URLdecode(cookie$value)))
   counts <- httr::content(result)$"values-response"$"distinct-value"
 
-  NTrackTypes <- length(counts)
-  track_counts <- data.frame()
-  if (NTrackTypes != 0) {
+  if (is.null(hierarchy)) {
     h <- sensingcluesr::get_hierarchy(url = url, lang = lang)
-    for (i in 1:NTrackTypes) {
-      id <- counts[[i]]$'_value' # e.g. https://sensingclues.poolparty.biz/SCCSSOntology/0
-      parent <- sensingcluesr::get_parent_id(id, h)
-      # first check if the parent concept is found
-      # then filter the concepts that fall under the top concept for tracks
-      if (!is.null(parent) && parent == "https://sensingclues.poolparty.biz/SCCSSOntology/42") { # concept 42 = Tracks
-        name <- sensingcluesr::get_label(id, h)
-        count <- counts[[i]]$frequency
-        track_counts <- rbind(track_counts, c(id, name, count))
-      }
-    }
-    # names
-    names(track_counts) <- c("id", "name", "count")
+  } else {h <- hierarchy}
+
+  if (length(counts) > 0) {
+    track_counts <- data.frame(
+      id = sapply(counts, function(x) x$`_value`),
+      name = sapply(counts, function(x) sensingcluesr::get_label(x$`_value`, h)),
+      count = sapply(counts, function(x) x$frequency)
+    )
   } else {
     message("No track counts received from backend returning NULL")
     track_counts <- NULL
